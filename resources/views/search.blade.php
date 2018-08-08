37f8ae838883e71a7f68d9e7c82f49524761265a
Ferdinand Bryle Baguio <baguioferdinand@gmail.com>
2018-08-08 18:29:42 +0800

    Filter Added; search bar

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -10,17 +10,5 @@
 		 
 		</div>
 	{!! Form::close() !!}
-{!! Form::open(['method'=>'GET','url'=>url()->current(),'class'=>'navbar-form navbar-left'])  !!}
 
-
-<div class="form-inline">
-	{!! Form::inputGroup('text',null, 'name' ,null , ['class' => 'form-group' , 'placeholder' => 'Name']) !!}
-	{!! Form::inputGroup('email',null, 'email',null , ['class' => 'form-group' , 'placeholder' => 'Email']) !!}
-	{!! Form::selectGroup(null,'role',['' => 'Select Type', 'Admin' => 'Admin', 'Agent' => 'Agent'], null, ['class' => 'role']) !!}
-    <button class="btn btn-default" type="submit">
-            <i class="fa fa-search"></i>
-    </button>
-   
-</div>
-{!! Form::close() !!}
 
